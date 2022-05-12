e16e9511075d7748ebe03f12144770751370ac7d
Cristofer Ulate <v-culate@microsoft.com>
2022-05-12 16:20:54 +0000

    Initial commit

---
+++ CrisXUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace CrisXUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int val = 5;
+            // si los numeros son diferentes =  funciona
+            // si los numeros son iguales = falla
+            Debug.Assert(val != 2, "Value should not be 2.");
+        
+        }
+    }
+}
